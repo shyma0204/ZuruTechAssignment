6daadb0cc51a9e1c055c1899943e4db596c82af2
IlyaShyma <101025623+IlyaShyma@users.noreply.github.com>
2024-12-04 00:16:38 +0300

    - Implemented the --filter=<option> argument to filter directory contents by type (file or dir). - Added a logger system to replace print statements and enhance debugging and traceability. - Refactored methods for improved clarity, modularity, and adherence to best practices. - Small fixes here and there

--- pyls/__main__.py
+++ pyls/__main__.py
@@ -1,30 +1,37 @@
-import sys
 import argparse
-from .utils import load_json_file, list_contents, format_file_details
+import logging
+from .utils import load_json_file, filter_directory_contents, sort_directory_contents, \
+    print_directory_items
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler("app.log")
+    ]
+)
+
+logger = logging.getLogger(__name__)
 
 
-def main():
+def parse_arguments():
     parser = argparse.ArgumentParser(description='processing arguments')
-
-    parser.add_argument('--path', type=str, help='Path to the json file', default='./structure.json')
-    parser.add_argument('-A', action='store_true', help='Include hidden files ')
+    parser.add_argument('--path', type=str, help='Path to the JSON file', default='./file_system_structures/structure.json')
+    parser.add_argument('-A', action='store_true', help='Include hidden files')
     parser.add_argument('-l', action='store_true', help='Show detailed file information')
     parser.add_argument('-r', action='store_true', help='Reverse the order of the listing')
     parser.add_argument('-t', action='store_true', help='Sort by time modified')
+    parser.add_argument('--filter', type=str, choices=['file', 'dir'], help="Filter the results by 'file' or 'dir'.")
+    return parser.parse_args()
 
-    args = parser.parse_args()
+def main():
+    args = parse_arguments()
 
-    # print(f"Using JSON structure path: {args.path}")
-    directory_structure = load_json_file(args.path)
-
-    directory_items_to_show = list_contents(directory_structure, include_hidden=args.A, reverse=args.r,
-                                            sort_by='time_modified' if args.t else 'name')
-
-    if args.l:
-        for item in directory_items_to_show:
-            print(format_file_details(item))
-    else:
-        print(" ".join([item['name'] for item in directory_items_to_show]))
+    directory = load_json_file(args.path)
+    directory_items_to_show = filter_directory_contents(directory, filter_type=args.filter, include_hidden=args.A)
+    directory_items_to_show = sort_directory_contents(directory_items_to_show, sort_by='time_modified' if args.t else 'name',
+                                               reverse=args.r)
+    print_directory_items(directory_items_to_show, show_detailed_info=args.l)
 
 if __name__ == '__main__':
     main()
--- pyls/utils.py
+++ pyls/utils.py
@@ -1,7 +1,9 @@
 import json
 import sys
 from datetime import datetime, timezone
+import  logging
 
+logger = logging.getLogger(__name__)
 
 def load_json_file(file_path: str) -> dict[str, any]:
     """
@@ -15,37 +17,83 @@ def load_json_file(file_path: str) -> dict[str, any]:
     """
     try:
         with open(file_path, 'r') as file:
-            return json.load(file)
+            data = json.load(file)
+            logger.info("JSON file loaded successfully: %s", file_path)
+            return data
     except FileNotFoundError:
-        print(f"Error: File {file_path} not found.")
+        logger.error("File not found: %s", file_path)
         sys.exit(1)
     except json.JSONDecodeError:
-        print("Error: Failed to decode JSON.")
+        logger.error("Failed to decode JSON: %s", file_path)
         sys.exit(1)
     except Exception as e:
-        print(f"Error: {e}")
+        logger.critical("Unexpected error: %s", e)
         sys.exit(1)
 
 
-def list_contents(directory: dict[str, any], include_hidden: bool = False, reverse: bool = False,
-                  sort_by: str = 'name') -> list[dict[str, any]]:
+def sort_directory_contents(directory_items: list[dict[str, any]], sort_by: str = 'name', reverse: bool = False) -> \
+        list[dict[str, any]]:
     """
-    Lists the contents of the given directory.
+    Sorts a list of directory items by a given key and optionally reverses the order.
+
+    Args:
+        directory_items (list[dict[str, any]]): List of directory items to sort.
+        sort_by (str): The key to sort by ('name' or 'time_modified').
+        reverse (bool): Whether to reverse the sorting order.
+
+    Returns:
+        list[dict[str, any]]: Sorted list of directory items.
+    """
+
+    logger.info("Sorting directory items by %s, reverse=%s", sort_by, reverse)
+
+    return sorted(directory_items, key=lambda x: x[sort_by], reverse=reverse)
+
+
+def print_directory_items(directory_items: list[dict[str, any]], show_detailed_info: bool = False) -> None:
+    """
+    Prints a list of directory items with optional detailed information.
+
+    Args:
+        directory_items (list[dict[str, any]]): List of directory items to print.
+        show_detailed_info (bool): Whether to show detailed information.
+    """
+    logger.info("Printing directory items. Detailed info: %s", show_detailed_info)
+
+    if show_detailed_info:
+        for item in directory_items:
+            print(format_file_details(item))
+    else:
+        print(" ".join([item['name'] for item in directory_items]))
+
+
+def filter_directory_contents(directory: dict[str, any], filter_type: str = None,
+                              include_hidden: bool = False) -> list[dict[str, any]]:
+    """
+    Filters a given directory content by type ('file' or 'dir') and optionally includes hidden files.
 
     Args:
         directory (dict[str, any]): A dictionary representing the directory.
+        filter_type (str): The type to filter by ('file' or 'dir').
         include_hidden (bool): Whether to include hidden files (starting with '.').
-        reverse (bool): Whether to reverse the sorting order.
-        sort_by (str): The key to sort by ('name' or 'time_modified').
 
     Returns:
-        list[dict[str, any]]: Sorted list of directory contents.
+        list[dict[str, any]]: Filtered list of directory contents.
     """
+    logger.info("Filtering directory contents. Filter: %s, Include hidden: %s", filter_type, include_hidden)
+
     contents = directory.get('contents', [])
 
     if not include_hidden:
         contents = [item for item in contents if not item['name'].startswith('.')]
-    return sorted(contents, key=lambda x: x[sort_by], reverse=reverse)
+
+    if filter_type:
+        if filter_type == 'file':
+            contents = [item for item in contents if 'contents' not in item]
+        elif filter_type == 'dir':
+            contents = [item for item in contents if 'contents' in item]
+
+    return contents
 
 
 def format_file_details(file_item: dict[str, any]) -> str:
@@ -56,7 +104,7 @@ def format_file_details(file_item: dict[str, any]) -> str:
         file_item (dict[str, any]): The file or directory item as a dictionary.
 
     Returns:
-        str: A formatted string with file details.
+        str: A formatted string with file or directory details.
     """
     permissions = file_item.get('permissions', '-' * 10)
     size = file_item.get('size', 0)
